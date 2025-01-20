674b6da5272a554ed6308592b90a8a94d93900a7
Amar-Omerika <amar.omerika@edu.fit.ba>
2025-01-20 23:28:10 +0100

    feat: set up typescript and electron builder

---
+++ src/electron/main.ts
@@ -0,0 +1,14 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+app.on("ready", () => {
+  let mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  //set up dynamic path
+  mainWindow.loadFile(path.join(app.getAppPath() + "/dist-react/index.html"));
+});
