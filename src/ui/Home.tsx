818abdeb3f5ce2f668d19f1e966e002bf95434ad
Amar-Omerika <amar.omerika@edu.fit.ba>
2025-01-27 16:15:58 +0100

    feat: finish datepicker and add some functionallity

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import {
   Container,
   Button,
@@ -8,6 +8,10 @@ import {
   FormControl,
   InputLabel,
 } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 import { styled } from "@mui/system";
 import { COLORS } from "../ui/constants/constants";
@@ -18,49 +22,73 @@ const StyledContainer = styled(Container)({
 
 const Home = () => {
   const [section, setSection] = useState<string>("");
+  const [imePrezime, setImePrezime] = useState<string>("");
+  const [selectedYear, setSelectedYear] = useState<Dayjs | null>(dayjs());
 
   const handleChange = (event: ChangeEvent<{ value: unknown }>) => {
     setSection(event.target.value as string);
+    console.log(section);
   };
+  const handleYearChange = (newValue: Dayjs | null) => {
+    setSelectedYear(newValue);
+  };
+
   return (
-    <StyledContainer>
-      <div
-        style={{ display: "flex", justifyContent: "end", marginTop: "10px" }}
-      >
-        <Button style={{ backgroundColor: COLORS.primary, color: "#fff" }}>
-          Dodaj Korisnika
-        </Button>
-      </div>
-      <Container
-        maxWidth="xl"
-        disableGutters
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginTop: "10px",
-        }}
-      >
-        <TextField id="outlined-basic" label="Ime Prezime" variant="outlined" />
-        <FormControl variant="outlined" sx={{ minWidth: 200 }}>
-          <InputLabel id="section-label">Sekcija</InputLabel>
-          <Select
-            labelId="section-label"
-            id="section-select"
-            value={section}
-            onChange={() => handleChange}
-            label="Sekcija"
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Stari - Grad</MenuItem>
-            <MenuItem value={20}>Fejiceva</MenuItem>
-            <MenuItem value={30}>Spanski Logor</MenuItem>
-          </Select>
-        </FormControl>
-      </Container>
-    </StyledContainer>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StyledContainer>
+        <div
+          style={{ display: "flex", justifyContent: "end", marginTop: "10px" }}
+        >
+          <Button style={{ backgroundColor: COLORS.primary, color: "#fff" }}>
+            Dodaj Korisnika
+          </Button>
+        </div>
+        <Container
+          maxWidth="xl"
+          disableGutters
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: "20px",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Ime Prezime"
+            variant="outlined"
+            onChange={(e) => setImePrezime(e.target.value)}
+          />
+          <FormControl variant="outlined" sx={{ minWidth: 200 }}>
+            <InputLabel id="section-label">Sekcija</InputLabel>
+            <Select
+              labelId="section-label"
+              id="section-select"
+              value={section}
+              onChange={handleChange as any}
+              label="Sekcija"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Stari - Grad</MenuItem>
+              <MenuItem value={20}>Fejiceva</MenuItem>
+              <MenuItem value={30}>Spanski Logor</MenuItem>
+            </Select>
+          </FormControl>
+          <DatePicker
+            views={["year"]}
+            label="Odaberi Godinu"
+            value={selectedYear}
+            onChange={handleYearChange}
+            renderInput={(params: Dayjs | null) => <TextField {...params} />}
+          />
+          <Button style={{ backgroundColor: COLORS.primary, color: "#fff" }}>
+            Pretrazi
+          </Button>
+        </Container>
+      </StyledContainer>
+    </LocalizationProvider>
   );
 };
 
