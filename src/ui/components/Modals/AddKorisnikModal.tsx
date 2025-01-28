dc9a97dd98cca30500ba9d1252d284024ea1f722
Amar-Omerika <amar.omerika@edu.fit.ba>
2025-01-28 15:29:33 +0100

    refactore: code in add modal component

--- src/ui/components/Modals/AddKorisnikModal.tsx
+++ src/ui/components/Modals/AddKorisnikModal.tsx
@@ -10,8 +10,12 @@ import {
   Typography,
   Modal,
   Paper,
+  Autocomplete,
 } from "@mui/material";
-
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
 interface AddKorisnikModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -23,14 +27,12 @@ const AddKorisnikModal: React.FC<AddKorisnikModalProps> = ({
   onClose,
   onCreate,
 }) => {
+  const [selectedYear, setSelectedYear] = useState<Dayjs | null>(dayjs());
   const [korisnikInfo, setKorisnikInfo] = useState({
     brojRegistra: "",
-    imePrezime: "",
     iznosKM: "",
     adresaStanovanja: "",
     kontaktTelefon: "",
-    jmbg: "",
-    sekcija: "",
     status: "",
     napomena: "",
     prijava: "",
@@ -38,12 +40,9 @@ const AddKorisnikModal: React.FC<AddKorisnikModalProps> = ({
 
   const [errors, setErrors] = useState({
     brojRegistra: "",
-    imePrezime: "",
     iznosKM: "",
     adresaStanovanja: "",
     kontaktTelefon: "",
-    jmbg: "",
-    sekcija: "",
     status: "",
     napomena: "",
     prijava: "",
@@ -72,123 +71,132 @@ const AddKorisnikModal: React.FC<AddKorisnikModalProps> = ({
       toast.error("Greska pri dodavanju korisnika");
     }
   };
-
+  const handleYearChange = (newValue: Dayjs | null) => {
+    setSelectedYear(newValue);
+  };
   return ReactDOM.createPortal(
-    <AnimatePresence>
-      {isOpen && (
-        <Modal open={isOpen} onClose={onClose}>
-          <motion.div
-            style={{
-              position: "fixed",
-              inset: 0,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
-            }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <AnimatePresence>
+        {isOpen && (
+          <Modal open={isOpen} onClose={onClose}>
             <motion.div
-              initial={{ scale: 0.8, opacity: 0 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: 0.8, opacity: 0 }}
+              style={{
+                position: "fixed",
+                inset: 0,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
             >
-              <Paper sx={{ p: 4, borderRadius: 2 }}>
-                <Typography variant="h6" gutterBottom>
-                  Dodaj Korisnika
-                </Typography>
-                <Box component="form" sx={{ display: "grid", gap: 2 }}>
-                  <TextField
-                    label="Broj Registra"
-                    name="brojRegistra"
-                    value={korisnikInfo.brojRegistra}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <TextField
-                    label="Ime Prezime"
-                    name="imePrezime"
-                    value={korisnikInfo.imePrezime}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <TextField
-                    label="Iznos KM"
-                    name="iznosKM"
-                    value={korisnikInfo.iznosKM}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <TextField
-                    label="Adresa Stanovanja"
-                    name="adresaStanovanja"
-                    value={korisnikInfo.adresaStanovanja}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <TextField
-                    label="Kontakt Telefon"
-                    name="kontaktTelefon"
-                    value={korisnikInfo.kontaktTelefon}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <TextField
-                    label="JMBG"
-                    name="jmbg"
-                    value={korisnikInfo.jmbg}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <TextField
-                    label="Sekcija"
-                    name="sekcija"
-                    value={korisnikInfo.sekcija}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <TextField
-                    label="Status"
-                    name="status"
-                    value={korisnikInfo.status}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <TextField
-                    label="Napomena"
-                    name="napomena"
-                    value={korisnikInfo.napomena}
-                    onChange={handleChange}
-                    fullWidth
-                    multiline
-                    rows={4}
-                  />
-                  <TextField
-                    label="Prijava"
-                    name="prijava"
-                    value={korisnikInfo.prijava}
-                    onChange={handleChange}
-                    fullWidth
-                  />
-                  <Box
-                    sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}
-                  >
-                    <Button onClick={onClose} sx={{ mr: 2 }}>
-                      Prekini
-                    </Button>
-                    <Button variant="contained" onClick={handleCreate}>
-                      Dodaj
-                    </Button>
+              <motion.div
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.8, opacity: 0 }}
+              >
+                <Paper sx={{ p: 4, borderRadius: 2 }}>
+                  <Typography variant="h6" gutterBottom>
+                    Dodaj Korisnika
+                  </Typography>
+                  <Box component="form" sx={{ display: "grid", gap: 2 }}>
+                    <DatePicker
+                      views={["year"]}
+                      label="Odaberi Godinu"
+                      value={selectedYear}
+                      onChange={handleYearChange}
+                      renderInput={(params: Dayjs | null) => (
+                        <TextField {...params} />
+                      )}
+                    />
+                    <Autocomplete
+                      disablePortal
+                      options={[
+                        { label: "Saudin Guja", id: 1 },
+                        { label: "Mujo Mujkovic", id: 2 },
+                      ]}
+                      sx={{ width: 300 }}
+                      renderInput={(params) => (
+                        <TextField {...params} label="Korisnik" />
+                      )}
+                    />
+                    <TextField
+                      label="Broj Registra"
+                      name="brojRegistra"
+                      value={korisnikInfo.brojRegistra}
+                      onChange={handleChange}
+                      fullWidth
+                    />
+
+                    <TextField
+                      label="Iznos KM"
+                      name="iznosKM"
+                      value={korisnikInfo.iznosKM}
+                      onChange={handleChange}
+                      fullWidth
+                    />
+                    <TextField
+                      label="Adresa Stanovanja"
+                      name="adresaStanovanja"
+                      value={korisnikInfo.adresaStanovanja}
+                      onChange={handleChange}
+                      fullWidth
+                    />
+                    <TextField
+                      label="Kontakt Telefon"
+                      name="kontaktTelefon"
+                      value={korisnikInfo.kontaktTelefon}
+                      onChange={handleChange}
+                      fullWidth
+                    />
+
+                    <TextField
+                      label="Status"
+                      name="status"
+                      value={korisnikInfo.status}
+                      onChange={handleChange}
+                      fullWidth
+                    />
+                    <TextField
+                      label="Napomena"
+                      name="napomena"
+                      value={korisnikInfo.napomena}
+                      onChange={handleChange}
+                      fullWidth
+                      multiline
+                      rows={4}
+                    />
+                    <TextField
+                      label="Prijava"
+                      name="prijava"
+                      value={korisnikInfo.prijava}
+                      onChange={handleChange}
+                      fullWidth
+                    />
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        mt: 2,
+                      }}
+                    >
+                      <Button onClick={onClose} sx={{ mr: 2 }}>
+                        Prekini
+                      </Button>
+                      <Button variant="contained" onClick={handleCreate}>
+                        Dodaj
+                      </Button>
+                    </Box>
                   </Box>
-                </Box>
-              </Paper>
+                </Paper>
+              </motion.div>
             </motion.div>
-          </motion.div>
-        </Modal>
-      )}
-    </AnimatePresence>,
+          </Modal>
+        )}
+      </AnimatePresence>
+    </LocalizationProvider>,
     document.getElementById("modal-root")!
   );
 };
